234e99e14ae874246eade4c46a2c09faf1d25699
Space-wolf <952867436@qq.com>
2018-11-04 23:59:43 +0800

    首页初步完成

--- js/xlmove.js
+++ js/xlmove.js
@@ -4,6 +4,11 @@ $(function(){
         $('.probox .pro_item').eq($(this).index()).addClass('show').siblings().removeClass('show');
     })
 
+    $(".news_tits span").click(function(){
+        $(this).addClass('protit_bg').siblings().removeClass('protit_bg');
+        $('.news_box .news_category').eq($(this).index()).addClass('show2').siblings().removeClass('show2');
+    })
+
      
     
     
