f67b434ebc9d27c735352ed9ac5c6d08957586cf
Space-wolf <952867436@qq.com>
2018-11-04 16:24:26 +0800

    完成产品模块

--- js/xlmove.js
+++ js/xlmove.js
@@ -1,7 +1,7 @@
 $(function(){
-    $(".case_head_right span").click(function(){
-        $(this).addClass('show_border').siblings().removeClass('show_border');
-        $('.imgs_box .imgs').eq($(this).index()).addClass('show').siblings().removeClass('show');
+    $(".pro_list_tits span").click(function(){
+        $(this).addClass('protit_bg').siblings().removeClass('protit_bg');
+        $('.probox .pro_item').eq($(this).index()).addClass('show').siblings().removeClass('show');
     })
 
      
